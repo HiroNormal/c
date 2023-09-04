3556d351075a1071e120463afe99a2c604e81ee8
HiroNormal <hearthwave@gmail.com>
2023-09-04 19:01:39 +0100

    Add files via upload

--- index.c
+++ index.c
@@ -1,24 +1,19 @@
 #include <stdio.h>
-#include <stdbool.h> //libary that stores true or false
+#include <math.h>
 
 int main ()
 {
-   float pi = 3.14159; // float uses 4 bytes
-   double longfloat = 3.14159; // 8 bytes more percise
+    double pi = 3.14159;
+    double radius;
+    double circumference;
 
-   char name[] = ("Bro"); 
-   char grade = 'c'; //(-128 to 127)
-   unsigned char littlenum = 255; // (0 to 255)
+    
 
-   short int shortint = 31415; // 2 bytes (-32,768 to 32,767)
-   unsigned short unshortint = 31415 ; // 2 bytes (65,535)
+     printf("i will calulate the circumference of your , give me the radius of your tire\n");
+     scanf("%lf",&radius);
 
-   bool boolean = true;
+     circumference = 2 * pi * radius;
 
-   printf("your name is %s\n",name); //  %s for String
-   printf("your grade is %c\n",grade); //  %c for character
-   printf("your Gpa is %.4f\n",pi); //  %F for Float
-   printf("your number is %lf\n",longfloat); // %lf for fdouble
-   printf("%d \n",shortint); //  %d for Integer 
-   printf("%d", littlenum );
+     printf("the circumference is %lf",circumference);
+     return 0;
 }
