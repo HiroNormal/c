095d038661c89079b290fea7ebf3f447b5a98e9a
Dmitrijs <hearthwave@gmail.com>
2023-09-05 21:51:34 +0100

    added loop

--- index.c
+++ index.c
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #include <stdio.h>
 #include <math.h>
 
@@ -17,3 +18,42 @@ int main ()
      printf("the circumference is %lf",circumference);
      return 0;
 }
+=======
+#include <stdio.h>
+#include <math.h>
+#include <ctype.h>
+#include <stdbool.h>
+
+
+int main ()
+{
+    double pi = 3.14159;
+    double b;
+    double a;
+    double hypotanuse;
+    bool x = true;
+
+
+
+    while(x){
+        
+     printf("gimmie two numbers\n");
+     scanf("%lf",&b);
+     scanf("%lf",&a);
+     if(!isdigit(a) || !isdigit(b)) {
+        printf("error");
+        break;
+     } else {
+        hypotanuse = sqrt(a * b);
+        x = false;
+      printf("the hypotanuse is %lf",hypotanuse);
+
+     }
+
+    }
+
+
+    
+     return 0;
+}
+>>>>>>> 9d8258f (added while loop and error message)
